09e25a4c3cbcbfc7ab8557a5dd118d622c639b47
Preetham Praveen <preethampraveencode26@gmail.com>
2025-02-11 10:15:49 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SnakeGame from "./SnakeGame"; // Import your game component
-import "./styles.css"; // Ensure styles are included
+import "./SnakeGame.css"; // Ensure styles are included
 
 ReactDOM.render(
   <React.StrictMode>
